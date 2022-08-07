eaa83f8e9c4905bc0d710670d91085855d90260f
akira-yp <yog.puripuri@gmail.com>
2022-08-07 12:01:33 +0900

    loadingコンポーネントを作成

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,7 @@ import ja from "date-fns/locale/ja";
 import axios from 'axios';
 import addDays from 'date-fns/addDays'
 import Result from './Result';
+import Loading from './Loading';
 import './Common.css';
 
 export type Plan = {
@@ -27,16 +28,27 @@ const Home = () => {
   const [departure, setDeparture] = React.useState<number>(1);
   const [duration, setDuration] = React.useState<number>(60);
   const [plans, setPlans] = React.useState<Plan[]>([]);
-  const onFormSubmit = async (event:{preventDefault:() => void;}) => {
-    event.preventDefault();
-
-    const response = await axios.get("https://l1kwik11ne.execute-api.ap-northeast-1.amazonaws.com/production/golf-courses",{
-      params: { date: addDays(date,14), budget: budget, departure: departure, duration: duration }
-    });
-
-    setPlans(response.data.plans);
-  }
+  const [plansCount, setPlansCount] = React.useState<number | undefined>(undefined)
+  const [hasError, setHasError] = React.useState<boolean>(false);
+  const [loading, setLoading] = React.useState<boolean>(false);
   registerLocale('ja',ja);
+  const onFormSubmit = async (event:{preventDefault:() => void;}) => {
+    try {
+      event.preventDefault();
+      setLoading(true);
+      const response = await axios.get("https://l1kwik11ne.execute-api.ap-northeast-1.amazonaws.com/production/golf-courses",{
+        params: { date: addDays(date,14), budget: budget, departure: departure, duration: duration }
+      });
+
+      setPlans(response.data.plans);
+      setPlansCount(response.data.plansCount);
+      setLoading(false);
+    } catch(e){
+      console.log(e);
+      setHasError(true);
+    };
+  }
+
   return (
     <div className="ui container" id="container">
       <div className="Search__Form">
@@ -96,7 +108,9 @@ const Home = () => {
             </button>
           </div>
         </form>
-        <Result plans={plans} />
+        <Loading loading={loading} />
+
+        <Result plans={plans} plansCount={plansCount} error={hasError}/>
       </div>
     </div>
   );
